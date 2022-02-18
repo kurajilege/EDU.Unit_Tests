7f8f9a1498e501de76a21dc4d0e07f677dc7be55
Булат Салахутдинов <b.salakhutdinov@bars-open.ru>
2022-02-19 01:11:41 +0300

    Add project files.

---
+++ EDU.Unit_Tests/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EDU.Unit_Tests.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
